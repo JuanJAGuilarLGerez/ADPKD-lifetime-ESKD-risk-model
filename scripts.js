5cb00e9b1fee14be142cf08edd41f1ada4e7a3dd
JuanJAGuilarLGerez <146051532+JuanJAGuilarLGerez@users.noreply.github.com>
2024-11-17 21:57:49 -0600

    A

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.log('Service Worker registration failed:', error);
+    });
+
 var units_general = "";
 var units_creatinine = "";
 var sex = "";
